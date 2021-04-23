33578d74fda765f185735d8f60862f72487d69d3
Arvianto Dwi <arvi@invoicegenic.com>
2021-04-24 02:49:12 +0700

    🚧 API proof of concept
    - Fix local time to UTC using python3 recommended method

--- api/src/core/models.py
+++ api/src/core/models.py
@@ -8,8 +8,8 @@ class User(db.Model):
     id = db.Column(db.String(40), primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.now(tz=timezone.utc))
+    updated_at = db.Column(db.DateTime, default=datetime.now(tz=timezone.utc))
     deleted_at = db.Column(db.DateTime, nullable=True)
 
     def __repr__(self):
